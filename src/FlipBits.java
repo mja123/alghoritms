422534cf3bfd96b1ef8ff48058c96e8f53bb787e
mja123 <solvd@MacBook-Pro-de-solvd.local>
2022-08-11 12:04:54 -0300

    FlipBits and Pangram challenges solved

---
+++ src/FlipBits.java
@@ -0,0 +1,19 @@
+public class FlipBits {
+    public static long flippingBits(long n) {
+        //count of bits
+        int bits = 31;
+        long maxValue = 0;
+
+        //the biggest value of n bits
+        while (bits != -1) {
+            maxValue += Math.round(Math.pow(2, bits));
+            bits--;
+        }
+        //1 complement
+        return maxValue - n;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(flippingBits(3));
+    }
+}
