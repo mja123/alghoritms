520b0543ae17f9d3ff7095b537a7705abfdf7a39
mja123 <matias.aguilar.sansoni@gmail.com>
2022-08-26 11:36:00 -0300

    ArrayChange, PalindromeRearranging solved

---
+++ src/codeSignal/lvl4/ArrayChange.java
@@ -0,0 +1,31 @@
+package codeSignal.lvl4;
+
+public class ArrayChange {
+    static int solution(int[] inputArray) {
+        int counter = 0;
+        int increment = 0;
+        boolean smaller = false;
+
+        for (int i = 0; i < inputArray.length - 1; i++) {
+
+            if (smaller) {
+                inputArray[i] += increment;
+            }
+
+            if (inputArray[i] < inputArray[i + 1]) {
+                smaller = false;
+                continue;
+            }
+
+            increment = (inputArray[i] - inputArray[i + 1]) + 1;
+            counter += increment;
+            smaller = true;
+        }
+
+        return counter;
+    }
+    public static void main(String[] args){
+        int[] array = {1, 1, 1};
+        System.out.println(solution(array));
+    }
+}
