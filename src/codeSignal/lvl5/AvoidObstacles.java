87af5f0dce5f922029083f2e3d2d61788aac0d96
mja123 <matias.aguilar.sansoni@gmail.com>
2022-08-30 10:53:30 -0300

    Solution for BoxBlur and AvoidObstacles

--- src/codeSignal/lvl5/AvoidObstacles.java
+++ src/codeSignal/lvl5/AvoidObstacles.java
@@ -6,26 +6,23 @@ import java.util.Comparator;
 import java.util.List;
 
 public class AvoidObstacles {
-    static int solution(int[] inputArray) {
-        List<Integer> input = new ArrayList<>(Arrays.stream(inputArray).boxed().toList());
-        input.sort(Comparator.naturalOrder());
+    /*
+        Is not my solution, I could not solve it :(
+     */
+    int solution(int[] inputArray) {
+        int jump = 1;
+        boolean fail = true;
 
-        int length = input.contains(input.get(0) + 1) ? input.size(): input.get(0) + 1;
-
-        for (int i = length; i < input.size() - 1; i++) {
-            int adjacentDiff = input.get(i) - input.get(i + 1);
-            if (input.contains(input.get(i) + length)) {
-                length = adjacentDiff;
-            } else {
-                length = Math.min(adjacentDiff, length);
-            }
+        while(fail) {
+            jump++;
+            fail = false;
+            for (int j : inputArray)
+                if (j % jump == 0) {
+                    fail = true;
+                    break;
+                }
         }
-        return length;
+        return jump;
     }
 
-    public static void main(String[] args) {
-        int[] array = {5, 3, 6, 7, 9};
-
-        System.out.println(solution(array));
-    }
 }
---
+++ src/codeSignal/lvl5/BoxBlur.java
@@ -0,0 +1,52 @@
+package codeSignal.lvl5;
+
+public class BoxBlur {
+    // It's not my solution, I couldn't solve it.
+    int[][] solution(int[][] image) {
+        int[][] result = new int[image.length-2][image[0].length-2];
+        for (int i = 1; i < image.length-1; i++) {
+            for (int j = 1; j < image[i].length-1; j++) {
+                result[i-1][j-1] = avg(image, i, j);
+            }
+        }
+        return result;
+    }
+
+    int avg(int[][] image, int x, int y) {
+        int sum = 0;
+        for (int i = x-1; i <= x+1; i++) {
+            for (int j = y-1; j <= y+1; j++) {
+                sum += image[i][j];
+            }
+        }
+        return sum / 9;
+    }
+   /*static int[][] solution(int[][] image) {
+        int grade = ((image[0].length / 3) * (image[0].length / 3)) + (image[0].length % 3);
+        int[][] result = new int[grade][grade];
+
+
+        for (int i = 0; i < image.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < image[i].length; j++) {
+                sum += image[i][j];
+            }
+            result[image.length - 2 - i][image[i].length - 2 - i] = sum / 9;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+       int[][] input = {{0,18,9},
+                        {27,9,0},
+                        {81,63,45}};
+       int[][] answer = solution(input);
+        for (int[] ints : answer) {
+            for (int j = 0; j < answer[0].length; j++) {
+                System.out.print(ints[j] + " ");
+            }
+            System.out.println(" ");
+        }
+
+    }*/
+}
