962f0e6ac61bb7492cf0939c9c1f9c68529805f9
mja123 <matias.aguilar.sansoni@gmail.com>
2022-08-31 10:43:29 -0300

    MinesWeepers, EvenDigits, ArrayReplace solved

---
+++ src/codeSignal/lvl6/ArrayReplace.java
@@ -0,0 +1,12 @@
+package codeSignal.lvl6;
+
+public class ArrayReplace {
+    int[] solution(int[] inputArray, int elemToReplace, int substitutionElem) {
+        int[] answer = new int[inputArray.length];
+
+        for (int i = 0; i < inputArray.length; i++) {
+            answer[i] = (inputArray[i] == elemToReplace) ? substitutionElem : inputArray[i];
+        }
+        return answer;
+    }
+}
