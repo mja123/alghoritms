d8316bae3dbcfe8fa4930b213c4ad2ba5d14bffb
mja123 <matias.aguilar.sansoni@gmail.com>
2022-09-13 09:08:15 -0300

    Solved GrowingPlant

---
+++ src/codeSignal/lvl9/GrowingPlant.java
@@ -0,0 +1,33 @@
+package codeSignal.lvl9;
+
+public class GrowingPlant {
+//    Better solution:
+//             The height after x days (up - down) * (x - 1) + up
+//             We want dh <= (u - d) * (x - 1) + u
+//             (dh - u) / (u - d) + 1 <= x
+
+//            return Math.max(0, (int) Math.ceil((double) (desiredHeight - upSpeed) / (upSpeed - downSpeed))) + 1;
+    static int solution(int upSpeed, int downSpeed, int desiredHeight) {
+        int days = 0;
+        int height = 0;
+
+        while (height < desiredHeight) {
+            height += upSpeed;
+            days++;
+
+            if (height >= desiredHeight) {
+                break;
+            }
+            
+            height -= downSpeed;
+        }
+        return days;
+    }
+
+    public static void main(String[] args){
+        int upSpeed = 100;
+        int downSpeed = 10;
+        int desiredHeight = 910;
+        System.out.println(solution(upSpeed, downSpeed, desiredHeight));
+    }
+}
