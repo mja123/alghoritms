d37a8ad8ebd0a17efd266a4bfcdf9e601647b4f2
mja123 <matias.aguilar.sansoni@gmail.com>
2022-09-19 11:42:49 -0300

    lvl 9 finished

---
+++ src/codeSignal/lvl9/LongestDigitPrefix.java
@@ -0,0 +1,17 @@
+package codeSignal.lvl9;
+
+public class LongestDigitPrefix {
+    String solution(String inputString) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < inputString.length(); i++) {
+            if (Character.isDigit(inputString.charAt(i))) {
+                result.append(inputString.charAt(i));
+            } else {
+                break;
+            }
+        }
+        return result.toString();
+    }
+
+}
