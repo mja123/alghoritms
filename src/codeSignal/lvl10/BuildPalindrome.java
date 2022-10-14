417fd8a555c0bf29b9803941b593499b6af7fb50
mja123 <matias.aguilar.sansoni@gmail.com>
2022-10-14 10:25:15 -0300

    buildPalindrome solved

--- src/codeSignal/lvl10/BuildPalindrome.java
+++ src/codeSignal/lvl10/BuildPalindrome.java
@@ -1,41 +1,18 @@
 package codeSignal.lvl10;
-
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.stream.Stream;
-
 public class BuildPalindrome {
-    static String solution(String st) {
-        StringBuilder lettersAdded = new StringBuilder(st);
+    String solution(String st) {
+        String rs = new StringBuffer(st).reverse().toString();
+        String answer = "";
 
-        if (lettersAdded.reverse().toString().equals(st)) {
-            return st;
-        }
-        lettersAdded.reverse();
         for (int i = 0; i < st.length(); i++) {
-            for (int j = st.length() - 1; j > i; j--) {
-                if (j != st.indexOf((st.charAt(j)))) {
-                    continue;
-                }
-                if (st.charAt(i) == st.charAt(j)) {
 
-                    //Add at the end the letters before i
-                    if (i > (st.length() / 2) - 1) {
-
-
-                        lettersAdded.append(new StringBuilder(st.subSequence(0, i)).reverse());
-                        //Add at the begining the letters before j
-                    } else {
-                        lettersAdded.insert(0, new StringBuilder(st.subSequence(j, st.length())).reverse());
-                    }
-                    return lettersAdded.toString();
-                }
+            if (rs.startsWith(st.substring(i))) {
+                answer = st.substring(0, i) + rs;
+                break;
             }
         }
-        return lettersAdded.append(new StringBuilder(st.substring(0, st.length() - 1)).reverse()).toString();
-    }
 
-    public static void main(String[] args) {
-        System.out.println(solution("ababab"));
+        return answer;
     }
 }
+    
