d8523c626c6343ae45837fd5c1c2fa156a820b89
mja123 <matias.aguilar.sansoni@gmail.com>
2022-11-10 11:37:22 -0300

    Views added

--- src/hackerRank/Coins.java
+++ src/hackerRank/Coins.java
@@ -15,7 +15,7 @@ public class Coins {
     public static int combinations(int coins, int...products) {
         Set<String> combinations = new HashSet<>();
         StringBuilder combination = new StringBuilder();
-        
+
         for (int i : products) {
             //Spending all the budget in only one product.
             for (int j = 1; j < (coins / i) + 1; j++) {
