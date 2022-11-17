99260841b4b7e4044083c39a9b36eafa7f69f656
mja123 <matias.aguilar.sansoni@gmail.com>
2022-11-17 09:16:40 -0300

    Count the steps to guess a number between a range

---
+++ src/groockingAlgBook/BinarySearch.java
@@ -0,0 +1,34 @@
+package groockingAlgBook;
+
+public class BinarySearch {
+    static int solution (int s, int l, int h, int goal) {
+        int m = (l + h) / 2;
+        s++;
+
+        if (m == goal) {
+            return s;
+        }
+
+        /*
+        Goal = 20
+        s (steps) = 0
+        l (low)= 0
+        h (high)= 100
+        m (middle) = (h + l) / 2 = 50
+        s++
+        m > goal ? h = m : l = m
+         */
+
+        if (m > goal) {
+            h = m;
+        } else {
+            l = m;
+        }
+        return solution(s, l, h, goal);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(0, 0, 100, 20));
+    }
+}
